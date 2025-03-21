ca0e99af6e942abedb4210de46d550a40e14862e
João Pedro Santos <jpedroalmeida179@gmail.com>
2025-03-20 23:29:08 -0300

    instalando cors

--- api-tax-calculator/api.js
+++ api-tax-calculator/api.js
@@ -1,7 +1,11 @@
 import express from "express";
+import cors from "cors";
 
 const app = express();
 const PORT = 3000;
+
+app.use(cors());
+app.use(express.json());
 
 app.get("/", (req, res) => {
     res.send("Api funcionando!");
