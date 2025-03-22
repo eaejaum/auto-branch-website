f47c9de7dd86f9e376acf22ca08becb695a66440
João Pedro Santos <jpedroalmeida179@gmail.com>
2025-03-22 00:11:10 -0300

    contexto utilizando local storage para resgatar usuário

--- web-tax-calculator/src/context/authContext.jsx
+++ web-tax-calculator/src/context/authContext.jsx
@@ -1,29 +1,51 @@
-import { createContext, useContext, useState } from "react";
+import { createContext, useContext, useEffect, useState } from "react";
 
 export const AuthContext = createContext(null);
 
 export function AuthProvider({ children }) {
-    const [isAuthenticated, setIsAuthenticated] = useState(false);
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+  const [user, setUser] = useState(null);
+  const [error, setError] = useState(null);
 
-    function login() {
-        setIsAuthenticated(true);
-    };
+  useEffect(() => {
+    const storedUser = localStorage.getItem("user");
+    if (storedUser) {
+      const parsedUser = JSON.parse(storedUser);
+      setUser(parsedUser);
+      setIsAuthenticated(true);
+    }
+  }, []);
 
-    function logout() {
-        setIsAuthenticated(false);
-    };
+  async function login() {
+    try {
+      setUser({ name: "John Doe" });
+      setIsAuthenticated(true);
+      localStorage.setItem("user", JSON.stringify({ name: "John Doe" }));
+    } catch (error) {
+      setError(error);
+      setIsAuthenticated(false);
+    }
+  }
 
-    return  (
-        <AuthContext.Provider value={{ isAuthenticated, login, logout }}>
-            {children}
-        </AuthContext.Provider>
-    );
-};
+  function logout() {
+    setIsAuthenticated(false);
+    setUser(null);
+    localStorage.removeItem("user");
+  }
+
+  return (
+    <AuthContext.Provider
+      value={{ user, error, isAuthenticated, login, logout }}
+    >
+      {children}
+    </AuthContext.Provider>
+  );
+}
 
 export function useAuthContext() {
-    const context = useContext(AuthContext);
-    if (!context) {
-        throw new Error("useAuthContext must be used within an AuthProvider");
-    }
-    return context;
-};
+  const context = useContext(AuthContext);
+  if (!context) {
+    throw new Error("useAuthContext must be used within an AuthProvider");
+  }
+  return context;
+}
