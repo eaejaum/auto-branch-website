73efec7937162617064285588b55120c170668c7
João Pedro Santos <jpedroalmeida179@gmail.com>
2025-03-22 01:09:28 -0300

    consumindo rotas de usuário

--- api-tax-calculator/api.js
+++ api-tax-calculator/api.js
@@ -1,12 +1,15 @@
 import express from "express";
 import cors from "cors";
 import db from "./db/conn.js";
+import userRouter from "./routes/userRoutes.js";
 
 const app = express();
 const PORT = 3000;
 
 app.use(cors());
 app.use(express.json());
+
+app.use("/api/users", userRouter);
 
 app.get("/", (req, res) => {
     res.send("Api funcionando!");
