d14f11d3b7ecefcd5e7eff526ca1a971ea0309d9
João Pedro Santos <jpedroalmeida179@gmail.com>
2025-03-21 23:56:24 -0300

    roteamento base com proteção de rotas para usuários autenticados

--- web-tax-calculator/src/App.jsx
+++ web-tax-calculator/src/App.jsx
@@ -1,12 +1,24 @@
-import './App.css'
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import Home from "./pages/Home/Home";
+import NotFound from "./pages/404/NotFound";
+import "./App.css";
+import Login from "./auth/Login/Login";
+import PrivateRoute from "./components/PrivateRoute";
 
 function App() {
-
   return (
-    <>
-      <h1>Tax Calculator</h1>
-    </>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Navigate to="/login" />} />
+        <Route path="/login" element={<Login />} />
+
+        <Route element={<PrivateRoute />}>
+          <Route path="/home" element={<Home />} />
+          <Route path="*" element={<NotFound />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
---
+++ web-tax-calculator/src/auth/Login/Login.jsx
@@ -0,0 +1,21 @@
+import { useNavigate } from "react-router-dom";
+import { useAuthContext } from "../../context/authContext";
+
+function Login() {
+    const navigate = useNavigate();
+    const { login } = useAuthContext();
+
+    function handleLogin() {
+        login();
+        navigate("/home");
+    };
+
+    return (
+        <>
+            <h1>Login</h1>
+            <button onClick={handleLogin}>Entrar</button>
+        </>
+    )
+};
+
+export default Login;
