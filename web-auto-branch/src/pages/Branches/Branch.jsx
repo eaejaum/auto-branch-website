9d40240d4bc08d2919fd8b507a7f32c0ad469075
João Pedro Santos <jpedroalmeida179@gmail.com>
2025-05-23 22:41:23 -0300

    carregando dados de concessionarias, e card

--- web-auto-branch/src/pages/Branches/Branch.jsx
+++ web-auto-branch/src/pages/Branches/Branch.jsx
@@ -1,17 +1,23 @@
 import { useNavigate } from "react-router-dom";
 import { useAuthContext } from "../../context/authContext";
-import { Box, Button, Container, Flex } from "@radix-ui/themes";
+import { Box, Button, Flex, Spinner } from "@radix-ui/themes";
 import { Plus } from "lucide-react";
 import styles from "./Branch.module.css";
 import Navbar from "../../components/Navbar";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import CreateBranchModal from "../Branches/components/CreateBranchModal";
+import BranchCard from "./components/BranchCard";
+import { useBranchContext } from "../../context/branchContext";
 
 function Branch() {
     // const navigate = useNavigate();
+    const { loading, branches, getAllBranches } = useBranchContext();
     const [isAddModalOpen, setIsAddModalOpen] = useState(false);
     // const { logout } = useAuthContext();
 
+    useEffect(() => {
+        getAllBranches();
+    }, []);
     // function handleLogout() {
     //     logout();
     //     navigate("/login");
@@ -25,6 +31,17 @@ function Branch() {
                     <h1 className={styles.title}>Lista de Concessionárias</h1>
                     <Button onClick={() => setIsAddModalOpen(true)} className={styles.addBranchButton}><Plus color="#FFF" height={14} width={14} /> Nova Concessionária</Button>
                 </Flex>
+                {loading ? (
+                    <Flex justify="center" align="center">
+                        <Spinner />
+                    </Flex>
+                ) : (
+                    <Flex justify="start" align="start" gap="5">
+                        {!loading && branches && branches.map((branch) => (
+                            <BranchCard branch={branch} />
+                        ))}
+                    </Flex>
+                )}
             </Box>
             <CreateBranchModal open={isAddModalOpen} onOpenChange={setIsAddModalOpen} />
             {/* <Button onClick={handleLogout}>Logout</Button> */}
--- web-auto-branch/src/pages/Branches/components/BranchCard.jsx
+++ web-auto-branch/src/pages/Branches/components/BranchCard.jsx
@@ -1,6 +1,42 @@
-function BranchCard() {
+import { formatCep } from "../../../utils/formatCep";
+import styles from "./BranchCard.module.css";
+
+function BranchCard({ branch }) {
     return (
-        <div>Branch Card</div>
+        <div className={styles.card}>
+            <h3 className={styles.title}>{branch.name}</h3>
+
+            <div className={styles.detailRow}>
+                <span className={styles.detailLabel}>Responsável</span>
+                <span className={styles.detailValue}>{''}</span>
+            </div>
+
+            <div className={styles.detailRow}>
+                <span className={styles.detailLabel}>Telefone</span>
+                <span className={styles.detailValue}>{''}</span>
+            </div>
+            <div className={styles.detailRow}>
+                <span className={styles.detailLabel}>Cidade</span>
+                <span className={styles.detailValue}>{branch.city}</span>
+            </div>
+
+            <div className={styles.detailRow}>
+                <span className={styles.detailLabel}>Estado</span>
+                <span className={styles.detailValue}>{branch.state}</span>
+            </div>
+
+            <div className={styles.detailRow}>
+                <span className={styles.detailLabel}>CEP</span>
+                <span className={styles.detailValue}>{formatCep(branch.cep)}</span>
+            </div>
+
+            {/* Exemplo de campo com destaque */}
+            {branch.status && (
+                <div className={`${styles.detailValue} ${styles.highlight}`}>
+                    Status: {branch.status}
+                </div>
+            )}
+        </div>
     );
 };
 
