dd245184484e7cac68490351d81c6f5ddffb7472
João Pedro Santos <jpedroalmeida179@gmail.com>
2025-05-27 10:15:53 -0300

    tabelas atualizadas

---
+++ db_auto_branch/branches.sql
@@ -0,0 +1,8 @@
+CREATE TABLE branches (
+	id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    city VARCHAR(255) NOT NULL,
+    state VARCHAR(255) NOT NULL,
+    cep VARCHAR(255) NOT NULL UNIQUE,
+    phoneNumber VARCHAR(11);
+);
