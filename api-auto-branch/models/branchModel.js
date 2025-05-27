43eb259cc849c334cb445c2f2737aa23667b6af8
João Pedro Santos <jpedroalmeida179@gmail.com>
2025-05-27 10:17:00 -0300

    campo phoneNumber no back-end

--- api-auto-branch/models/branchModel.js
+++ api-auto-branch/models/branchModel.js
@@ -22,10 +22,10 @@ export const selectBranchById = async (id) => {
     }
 };
 
-export const insertBranch = async (name, city, state, cep) => {
+export const insertBranch = async (name, city, state, cep, phoneNumber) => {
     try {
-        const query = `INSERT INTO branches (name, city, state, cep) VALUES (?, ?, ?, ?)`;
-        const [results] = await db.promise().query(query, [name, city, state, cep]);
+        const query = `INSERT INTO branches (name, city, state, cep, phoneNumber) VALUES (?, ?, ?, ?, ?)`;
+        const [results] = await db.promise().query(query, [name, city, state, cep, phoneNumber]);
 
         return results;
     } catch (err) {
@@ -33,10 +33,10 @@ export const insertBranch = async (name, city, state, cep) => {
     }
 };
 
-export const updateBranch = async (id, name, city, state, cep) => {
+export const updateBranch = async (id, name, city, state, cep, phoneNumber) => {
     try {
-        const query = `UPDATE branches SET name = ?, city = ?, state = ?, cep = ? WHERE id = ?`;
-        const [results] = await db.promise().query(query, [name, city, state, cep, id]);
+        const query = `UPDATE branches SET name = ?, city = ?, state = ?, cep = ?, phoneNumber = ? WHERE id = ?`;
+        const [results] = await db.promise().query(query, [name, city, state, cep, phoneNumber, id]);
 
         return results.length > 0 ? results[0] : null;
     } catch (err) {
--- api-auto-branch/services/branchService.js
+++ api-auto-branch/services/branchService.js
@@ -14,23 +14,23 @@ export const getBranchByIdService = async (id) => {
     return branch;
 };
 
-export const createBranchService = async ({ name, city, state, cep }) => {
-    if (!name || !city || !state || !cep)
+export const createBranchService = async ({ name, city, state, cep, phoneNumber }) => {
+    if (!name || !city || !state || !cep || !phoneNumber)
         throw new AppError("Preencha os campos corretamente", 400);
 
-    return await insertBranch(name, city, state, cep);
+    return await insertBranch(name, city, state, cep, phoneNumber);
 };
 
-export const editBranchService = async ({ id, name, city, state, cep }) => {
+export const editBranchService = async ({ id, name, city, state, cep, phoneNumber }) => {
     const existingBranch = await selectBranchById(id);
 
     if (!existingBranch)
         throw new AppError("Concessionária não encontrada", 400);
 
-    if (!name || !city || !state || !cep)
+    if (!name || !city || !state || !cep || !phoneNumber)
         throw new AppError("Preencha os campos corretamente", 400);
 
-    return await updateBranch(id, name, city, state, cep);
+    return await updateBranch(id, name, city, state, cep, phoneNumber);
 };
 
 export const removeBranchService = async (id) => {
