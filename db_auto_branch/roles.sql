1743fd0ad490a9f1ce93629dff6590e96c0bd8b3
João Pedro Santos <jpedroalmeida179@gmail.com>
2025-06-18 23:19:32 -0300

    mudando Administrador para diretor

--- db_auto_branch/roles.sql
+++ db_auto_branch/roles.sql
@@ -3,6 +3,6 @@ CREATE TABLE roles (
     name VARCHAR(50) NOT NULL UNIQUE
 );
 
-INSERT INTO roles (name) VALUES ("Administrador");
+INSERT INTO roles (name) VALUES ("Diretor");
 INSERT INTO roles (name) VALUES ("Gerente");
 INSERT INTO roles (name) VALUES ("Vendedor");
