9e4d20ba7ffb5e4b68c444224c53a125e0c84368
João Pedro Santos <jpedroalmeida179@gmail.com>
2025-06-19 13:56:31 -0300

    concessionária nos detalhes do veículos

--- web-auto-branch/src/pages/Vehicles/components/VehicleDetails.jsx
+++ web-auto-branch/src/pages/Vehicles/components/VehicleDetails.jsx
@@ -1,6 +1,8 @@
 import Navbar from "../../../components/Navbar";
 import { Flex, Box, Text } from "@radix-ui/themes";
 import styles from "./VehicleDetails.module.css";
+import { formatPhoneNumber } from "../../../utils/formatPhoneNumber";
+import { formatCep } from "../../../utils/formatCep";
 import { useVehicleContext } from "../../../context/vehicleContext";
 import { useEffect, useState } from "react";
 import { useNavigate, useParams } from "react-router-dom";
@@ -55,68 +57,118 @@ function VehicleDetails() {
                     </Flex>
                 )}
                 {!loading && vehicle && (
-                    <Box className={styles.vehicleCard}>
-                        <Flex direction="column" gap="6">
-                            <Flex justify="between">
-                                <Flex direction="column">
-                                    <Text className={styles.vehicleName}>{(vehicle.brand).toUpperCase()} <span>{(vehicle.model).toUpperCase()}</span></Text>
-                                    <Text className={styles.vehicleEspecification}>{`${(vehicle.version).toUpperCase()} ${(vehicle.motorization).toUpperCase()} ${(vehicle.gearbox).toUpperCase()}`}</Text>
+                    <Flex direction="column" gap="6">
+                        <Box className={styles.card}>
+                            <Flex direction="column" gap="6">
+                                <Flex justify="between">
+                                    <Flex direction="column">
+                                        <Text className={styles.name}>{(vehicle.brand).toUpperCase()} <span>{(vehicle.model).toUpperCase()}</span></Text>
+                                        <Text className={styles.vehicleEspecification}>{`${(vehicle.version).toUpperCase()} ${(vehicle.motorization).toUpperCase()} ${(vehicle.gearbox).toUpperCase()}`}</Text>
+                                    </Flex>
+                                    <Flex gap="1">
+                                        <Text className={styles.vehiclePrice}>R${Number(vehicle.value).toFixed(0)}</Text>
+                                        <button className={styles.actionButton} onClick={() => setIsDeleteModalOpen(true)}>
+                                            <Trash width={15} height={15} color="#F3123C" />
+                                        </button>
+                                        <button className={styles.actionButton} onClick={() => setIsEditModalOpen(true)}>
+                                            <Edit width={15} height={15} color="#2563EB" />
+                                        </button>
+                                    </Flex>
                                 </Flex>
-                                <Flex gap="1">
-                                    <Text className={styles.vehiclePrice}>R${Number(vehicle.value).toFixed(0)}</Text>
-                                    <button className={styles.actionButton} onClick={() => setIsDeleteModalOpen(true)}>
-                                        <Trash width={15} height={15} color="#F3123C" />
-                                    </button>
-                                    <button className={styles.actionButton} onClick={() => setIsEditModalOpen(true)}>
-                                        <Edit width={15} height={15} color="#2563EB" />
+                                <Flex justify="between" align="end">
+                                    <div className={styles.infos}>
+                                        <Box>
+                                            <Flex direction="column">
+                                                <span className={styles.spanInfo}>Ano</span>
+                                                <Text className={styles.textInfo}>{vehicle.year}</Text>
+                                            </Flex>
+                                        </Box>
+                                        <Box>
+                                            <Flex direction="column">
+                                                <span className={styles.spanInfo}>Quilômetragem</span>
+                                                <Text className={styles.textInfo}>{Number(vehicle.km).toFixed(0)}</Text>
+                                            </Flex>
+                                        </Box>
+                                        <Box>
+                                            <Flex direction="column">
+                                                <span className={styles.spanInfo}>Câmbio</span>
+                                                <Text className={styles.textInfo}>{vehicle.gearbox}</Text>
+                                            </Flex>
+                                        </Box>
+                                        <Box>
+                                            <Flex direction="column">
+                                                <span className={styles.spanInfo}>Placa</span>
+                                                <Text className={styles.textInfo}>{vehicle.plate}</Text>
+                                            </Flex>
+                                        </Box>
+                                        <Box>
+                                            <Flex direction="column">
+                                                <span className={styles.spanInfo}>Cor</span>
+                                                <Text className={styles.textInfo}>{vehicle.color}</Text>
+                                            </Flex>
+                                        </Box>
+                                        <Box>
+                                            <Flex direction="column">
+                                                <span className={styles.spanInfo}>Estado</span>
+                                                <Text className={styles.textInfo}>{vehicle.branch.state}</Text>
+                                            </Flex>
+                                        </Box>
+                                    </div>
+                                    <button className={styles.sellButton}>
+                                        <Text className={styles.buttonText}>VENDER</Text>
                                     </button>
                                 </Flex>
                             </Flex>
-                            <Flex justify="between" align="end">
-                                <div className={styles.vehicleInfos}>
-                                    <Box>
-                                        <Flex direction="column">
-                                            <span className={styles.spanInfo}>Ano</span>
-                                            <Text className={styles.textInfo}>{vehicle.year}</Text>
-                                        </Flex>
-                                    </Box>
-                                    <Box>
-                                        <Flex direction="column">
-                                            <span className={styles.spanInfo}>Quilômetragem</span>
-                                            <Text className={styles.textInfo}>{Number(vehicle.km).toFixed(0)}</Text>
-                                        </Flex>
-                                    </Box>
-                                    <Box>
-                                        <Flex direction="column">
-                                            <span className={styles.spanInfo}>Câmbio</span>
-                                            <Text className={styles.textInfo}>{vehicle.gearbox}</Text>
-                                        </Flex>
-                                    </Box>
-                                    <Box>
-                                        <Flex direction="column">
-                                            <span className={styles.spanInfo}>Placa</span>
-                                            <Text className={styles.textInfo}>{vehicle.plate}</Text>
-                                        </Flex>
-                                    </Box>
-                                    <Box>
-                                        <Flex direction="column">
-                                            <span className={styles.spanInfo}>Cor</span>
-                                            <Text className={styles.textInfo}>{vehicle.color}</Text>
-                                        </Flex>
-                                    </Box>
-                                    <Box>
-                                        <Flex direction="column">
-                                            <span className={styles.spanInfo}>Estado</span>
-                                            <Text className={styles.textInfo}>{vehicle.branch.state}</Text>
-                                        </Flex>
-                                    </Box>
-                                </div>
-                                <button className={styles.sellButton}>
-                                    <Text className={styles.buttonText}>VENDER</Text>
-                                </button>
-                            </Flex>
-                        </Flex>
-                    </Box>
+                        </Box>
+                        <Box>
+                            <h1 className={styles.title}>Concessionária</h1>
+                            <Box className={styles.card}>
+                                <Flex direction="column" gap="6">
+                                    <Text className={styles.name}>{vehicle.branch.name}</Text>
+                                    <Flex justify="between" align="end">
+                                        <div className={styles.infos}>
+                                            <Box>
+                                                <Flex direction="column">
+                                                    <span className={styles.spanInfo}>Telefone</span>
+                                                    <Text className={styles.textInfo}>{formatPhoneNumber(vehicle.branch.phoneNumber)}</Text>
+                                                </Flex>
+                                            </Box>
+                                            <Box>
+                                                <Flex direction="column">
+                                                    <span className={styles.spanInfo}>Estoque</span>
+                                                    <Text className={styles.textInfo}>42 veículos</Text>
+                                                </Flex>
+                                            </Box>
+                                            <Box>
+                                                <Flex direction="column">
+                                                    <span className={styles.spanInfo}>Funcionários</span>
+                                                    <Text className={styles.textInfo}>12 Funcionários</Text>
+                                                </Flex>
+                                            </Box>
+                                            <Box>
+                                                <Flex direction="column">
+                                                    <span className={styles.spanInfo}>CEP</span>
+                                                    <Text className={styles.textInfo}>{formatCep(vehicle.branch.cep)}</Text>
+                                                </Flex>
+                                            </Box>
+                                            <Box>
+                                                <Flex direction="column">
+                                                    <span className={styles.spanInfo}>Estado</span>
+                                                    <Text className={styles.textInfo}>{vehicle.branch.state}</Text>
+                                                </Flex>
+                                            </Box>
+                                            <Box>
+                                                <Flex direction="column">
+                                                    <span className={styles.spanInfo}>Cidade</span>
+                                                    <Text className={styles.textInfo}>{vehicle.branch.state}</Text>
+                                                </Flex>
+                                            </Box>
+                                        </div>
+                                    </Flex>
+                                </Flex>
+                            </Box>
+                        </Box>
+                    </Flex>
                 )}
             </Box>
             <VehicleModal open={isEditModalOpen} onOpenChange={setIsEditModalOpen} vehicle={vehicle} refreshVehicle={refreshVehicle} />
